966d662b64f100c6667a2f7354d487eaa98b68cf
japannguyen <139685288+japannguyen@users.noreply.github.com>
2026-07-12 07:25:24 -0700

    chore: Remove wrongly pushed backend folder

--- backend/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../config/database');
-const User = require('./user.model');
-const Product = require('./product.model');
-
-// Associations
-User.hasMany(Product, { foreignKey: 'user_id', as: 'products' });
-Product.belongsTo(User, { foreignKey: 'user_id', as: 'owner' });
-
-module.exports = { sequelize, User, Product };
